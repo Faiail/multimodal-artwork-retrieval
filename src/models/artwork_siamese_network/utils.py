130c7bc02089df1fa115c046352778dde4eb51d2
Faiail <raffaele.scaringi20@gmail.com>
2024-03-06 14:51:47 +0100

    add new conf for printing val loss

--- src/models/artwork_siamese_network/utils.py
+++ src/models/artwork_siamese_network/utils.py
@@ -215,6 +215,7 @@ class Run:
                 cumulated_loss = cumulated_loss + loss
             if self.accelerator.is_main_process:
                 cumulated_loss = cumulated_loss.cpu().mean().item() / len(self.train_loader)
+                self.accelerator.print(f"Epoch {epoch}/{self.num_epochs}: training loss: {cumulated_loss:.4f}")
             self.early_stop(cumulated_loss, self.accelerator, self.model)
             self.scheduler.step(cumulated_loss)
             if self.early_stop.early_stop:
